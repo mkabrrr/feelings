3754c120b221a5558f1fa3a07e210b313fc80340
mkabrrr <70043854+mkabrrr@users.noreply.github.com>
2020-08-22 04:45:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NzQ2NDk4MTMzODQ4MjI3ODYx.X0BMkA.YQIc1ot5kgnxxDWwZ2z3kMvZ0Tc',    
+    TOKEN: '',    
     YT_API_KEY: 'api', 
     prefix: '$',
     devs: ['720857584646619211']
